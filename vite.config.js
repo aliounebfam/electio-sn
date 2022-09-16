eaff76df096143bea0cada544c3b6074e429446a
Alioune Badara FAM <aliounefam28@gmail.com>
2022-09-16 16:48:49 +0000

    add vite-plugin-pwa and configure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import manifest from './public/manifest.json'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: manifest
+    })
+  ]
 })
