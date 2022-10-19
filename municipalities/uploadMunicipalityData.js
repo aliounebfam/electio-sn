26f17b6a0f37a8d5526aa09f0f852043f160f751
Mohamed <mohamed.ndoye1@univ-thies.sn>
2022-10-19 14:18:05 +0000

    get all info geolocation for municipalities

---
+++ municipalities/uploadMunicipalityData.js
@@ -0,0 +1,28 @@
+import fetch from "node-fetch";
+import data from "./../assets/locations.json" assert { type: "json" };
+let values = data;
+
+Object.keys(values).forEach((element) => {
+  Object.keys(values[element]).forEach((el) => {
+    values[element][el].forEach((comm) => {
+      fetch(
+        "https://api.aladhan.com/v1/timingsByCity?city=" +
+          encodeURIComponent(comm) +
+          "&country=senegal&method=3"
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          values[element][el][values[element][el].indexOf(comm)] = {
+            [comm]: {
+              latitude: data.data.meta.latitude,
+              longitude: data.data.meta.longitude,
+              departement: el,
+            },
+          };
+        });
+    });
+  });
+});
